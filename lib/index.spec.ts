e86b45a80ddced50d31fd7d049da2c842d7e1f76
Gustaf Räntilä <g.rantila@gmail.com>
2021-02-18 23:12:23 +0100

    test(core): openapi to json schema document test

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -1,7 +1,8 @@
 import { JSONSchema7 } from 'json-schema'
 
 import { ensureValidOpenAPI } from '../test'
-import { jsonSchemaDocumentToOpenApi } from './index'
+import { jsonSchemaDocumentToOpenApi, openApiToJsonSchema } from './index'
+import { PartialOpenApiSchema } from './types'
 
 describe( "JSON Schema document to OpenAPI", ( ) =>
 {
@@ -20,6 +21,35 @@ describe( "JSON Schema document to OpenAPI", ( ) =>
 
 		ensureValidOpenAPI( openApiSchema );
 	} );
+} );
 
-	jsonSchemaDocumentToOpenApi
+describe( "OpenAPI Schema to JSON Schema document", ( ) =>
+{
+	it( "should convert null in document", ( ) =>
+	{
+		const schema: PartialOpenApiSchema = {
+			info: {
+				title: "the title",
+				version: "the version",
+			},
+			openapi: "3.0.0",
+			paths: { },
+			components: {
+				schemas: {
+					foo: {
+						type: "string",
+						nullable: true,
+						description: "Foo",
+					}
+				}
+			},
+		};
+		const jsonSchema = openApiToJsonSchema( schema );
+
+		expect( jsonSchema ).toStrictEqual( {
+			definitions: {
+				foo: { type: [ "string", "null" ], description: "Foo" }
+			}
+		} );
+	} );
 } );
